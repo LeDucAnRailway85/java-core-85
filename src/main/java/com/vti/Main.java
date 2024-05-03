839dca7915e304e08174d44f773b582072888a93
LeDucAnRailway85 <leducan16022001@gmail.com>
2024-05-04 00:37:40 +0900

    Create Project

--- src/main/java/com/vti/Main.java
+++ src/main/java/com/vti/Main.java
@@ -6,8 +6,6 @@ public class Main {
     public static void main(String[] args) {
         int a = 4;
         int b = 5;
-
-        a + b = c;
-        sysout
+        System.out.println("hell");
     }
 }
